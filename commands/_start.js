633743fc9413494a043fa06db7cd6c626f1e5c9c
Bots.Business <git.sync@bots.business>
2024-12-04 04:01:53 +0000

    Bots.Business export for bot id: 1807873

---
+++ commands/_start.js
@@ -0,0 +1,19 @@
+/*CMD
+  command: /start
+  help: You can ask me for whatever you want me to do, the keyboard is at the bottom 👇
+  need_reply: true
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+/name hello!
+
+Welcome to use New users can change faces once for free, and invited users can get more opportunities to change faces.
+
+🏁 Send /lang to switch the language
+  ANSWER
+  keyboard: desnudo total , bikini, colegiala , enfermera , solo bracier .
+  aliases: ⚠️ you have not followed the official channel yet. you can use the bot after. followinghttps://t.me/faceswap.
+  group: para mí 
+CMD*/
+
